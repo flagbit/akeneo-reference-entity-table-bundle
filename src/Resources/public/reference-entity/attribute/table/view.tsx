e97d307ffc41c99cd0cf308b397be8d47d4db207
Claudio Zizza <zizza@flagbit.de>
2020-06-10 17:24:35 +0200

    Implement modal functionality
    In the current situation it wasn't possible to use a modal for the table
    attribute, because the view is nested in the DOM where a z-index does't
    affect the whole window.

--- src/Resources/public/reference-entity/attribute/table/view.tsx
+++ src/Resources/public/reference-entity/attribute/table/view.tsx
@@ -4,6 +4,8 @@ import {getErrorsView} from 'akeneoreferenceentity/application/component/app/val
 import ValidationError from "akeneoreferenceentity/domain/model/validation-error";
 import Key from "akeneoreferenceentity/tools/key";
 import {TableAdditionalProperty, TableAttribute, TableProperty} from "./table";
+import {connect} from 'react-redux';
+import $ from 'jquery';
 
 /**
  * Here we define the React Component as a function with the following props :
@@ -16,12 +18,14 @@ import {TableAdditionalProperty, TableAttribute, TableProperty} from "./table";
  * It returns the JSX View to display the additional properties of your custom attribute.
  */
 const TableAttributeView = ({
-                               attribute,
-                               onAdditionalPropertyUpdated,
-                               onSubmit,
-                               errors,
-                               rights,
-                           }: {
+                                onTableEditionStart,
+                                attribute,
+                                onAdditionalPropertyUpdated,
+                                onSubmit,
+                                errors,
+                                rights
+                            }: {
+    onTableEditionStart: () => void
     attribute: TableAttribute;
     onAdditionalPropertyUpdated: (property: string, value: TableAdditionalProperty) => void;
     onSubmit: () => void;
@@ -41,32 +45,70 @@ const TableAttributeView = ({
     return (
         <React.Fragment>
             <div className="AknFieldContainer" data-code="table_property">
-                <div className="AknFieldContainer-header AknFieldContainer-header--light">
-                    <label className="AknFieldContainer-label" htmlFor="pim_reference_entity.attribute.edit.input.table_property">
-                        {__('pim_reference_entity.attribute.edit.input.table_property')}
-                    </label>
+                <div className="AknFieldContainer-header">
+                    <button onClick={onTableEditionStart} className="AknButton" data-code="table_property">
+                        {__('pim_reference_entity.attribute.edit.input.manage_options.quick_edit.label')}
+                    </button>
                 </div>
-                <div className="AknFieldContainer-inputContainer">
-                    <input
-                        type="text"
-                        autoComplete="off"
-                        className={inputTextClassName}
-                        id="pim_reference_entity.attribute.edit.input.table_property"
-                        name="table_property"
-                        readOnly={!rights.attribute.edit}
-                        value={attribute.table_property.normalize()}
-                        onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
-                            if (Key.Enter === event.key) onSubmit();
-                        }}
-                        onChange={(event: React.FormEvent<HTMLInputElement>) => {
-                            onAdditionalPropertyUpdated('table_property', new TableProperty(event.currentTarget.value));
-                        }}
-                    />
-                </div>
-                {getErrorsView(errors, 'table_property')}
             </div>
+            <div className="AknFieldContainer-header AknFieldContainer-header--light">
+                <label className="AknFieldContainer-label" htmlFor="pim_reference_entity.attribute.edit.input.table_property">
+                    {__('pim_reference_entity.attribute.edit.input.table_property')}
+                </label>
+            </div>
+            <div className="AknFieldContainer-inputContainer">
+                <input
+                    type="text"
+                    autoComplete="off"
+                    className={inputTextClassName}
+                    id="pim_reference_entity.attribute.edit.input.table_property"
+                    name="table_property"
+                    readOnly={!rights.attribute.edit}
+                    value={attribute.table_property.normalize().toString()}
+                    onKeyPress={(event: React.KeyboardEvent<HTMLInputElement>) => {
+                        if (Key.Enter === event.key) onSubmit();
+                    }}
+                    onChange={(event: React.FormEvent<HTMLInputElement>) => {
+                        onAdditionalPropertyUpdated('table_property', new TableProperty([event.currentTarget.value]));
+                    }}
+                />
+            </div>
+            {getErrorsView(errors, 'table_property')}
+
+            <TableAttributeModal />
         </React.Fragment>
     );
 };
 
-export const view = TableAttributeView;
+interface TableProp {
+}
+
+export class TableAttributeModal extends React.Component<TableProp> {
+    render() {
+        return (
+            <React.Fragment>
+                <div className="modal in flagbitTableAttribute" id="table" aria-hidden="false" style={{zIndex: 1042, display: 'none'}}>
+                    <div>
+                        <div className="AknFullPage AknFullPage--full">
+                            <div className="AknFullPage-content">
+                                <h1>My MODAL!!!!</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
+}
+
+export const view = connect(
+    () => ({}),
+    () => ({
+        onTableEditionStart: () => {
+            // dispatch({type: 'FLAGBIT_TABLE_EDITION_START', table_property: []});
+
+            $('#table').css({'display': 'block'});
+            $('#table').detach().prependTo('body .app:first');
+        },
+    })
+)(TableAttributeView);
