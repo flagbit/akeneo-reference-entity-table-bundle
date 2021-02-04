65ead285065714fa0a24ca383627ebbe858d7471
Claudio Zizza <zizza@flagbit.de>
2021-02-04 17:52:05 +0100

    Create test for localized select attribute

--- jest/public/reference-entity/attribute/table/type/test-helper.tsx
+++ jest/public/reference-entity/attribute/table/type/test-helper.tsx
@@ -2,17 +2,18 @@ import {
     ConfigChangeState,
     FlagbitTableTypes
 } from "../../../../../../src/Resources/public/reference-entity/attribute/table/type/type";
+import Locale from "akeneoreferenceentity//domain/model/locale";
 import {shallow} from "enzyme";
 import * as React from "react";
 
 
-function renderType(config, type: string, onchange: (config: object, index: number) => void) {
+function renderType(config, type: string, onchange: (config: object, index: number) => void, supportedLocales: Locale[] = []) {
     const configChangeState: ConfigChangeState = {
         typeCode: type,
         updateConfig: onchange,
         index: 0,
         config: config,
-        supportedLocales: [],
+        supportedLocales: supportedLocales,
     };
 
     const AttributeType = () => FlagbitTableTypes.typeRegistry.render(configChangeState);
